4a1bc47ac31a3c568ff3672d412518d7d8fc29df
Boi <51324966+LOL-BOI-LOL@users.noreply.github.com>
2021-11-17 03:57:14 +0000

    I added a system that makes sure audio is played and if it is not played it shows a pop-up that when clicked makes the audio play.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const mediaQuery = window.matchMedia('(max-width: 599px)');
 const divTopPH = document.getElementById('divTitlePlaceHolder');
 const divTop = document.getElementById('divTop');
+const audio = document.getElementById('Audio');
+const divPopUp = document.getElementById('divPopUp');
+const divBlur = document.getElementById('divBlur');
 document.getElementById('txtDropDown').addEventListener("click", toggleDropDown);
 window.addEventListener('resize', setPlaceHolderHeights);
+divPopUp.addEventListener('click', scrnUnblur);
 function toggleDropDown() {
   if(mediaQuery.matches) {
     let links = document.getElementById('divLinks').childNodes;
@@ -20,4 +24,16 @@ function toggleDropDown() {
 function setPlaceHolderHeights() {
   divTopPH.style.height = (100 * (parseInt(window.getComputedStyle(divTop).height.split('p').shift()) + 20) / window.innerWidth).toString() + 'vw';
 }
+function scrnUnblur() {
+  divPopUp.style.display = 'none';
+  divBlur.style.display = 'none';
+  audio.play();
+}
+function audioCheck() {
+  if(audio.paused) {
+    divPopUp.style.display = 'block';
+    divBlur.style.display = 'block';
+  }
+}
 setPlaceHolderHeights();
+setTimeout(audioCheck, 50);
