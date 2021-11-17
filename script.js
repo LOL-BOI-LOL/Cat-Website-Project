0b95a7ee705d4c9c36e93ccbd7ab30ab1a370dc1
Boi <51324966+LOL-BOI-LOL@users.noreply.github.com>
2021-11-17 17:23:15 +0000

    Added a toggle for music and got rid of pop-up, added the ability to enlarge an image, etc.

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const mediaQuery = window.matchMedia('(max-width: 599px)');
 const divTopPH = document.getElementById('divTitlePlaceHolder');
 const divTop = document.getElementById('divTop');
 const audio = document.getElementById('Audio');
+const imgEnlarged = document.getElementById('imgEnlarged');
 const divPopUp = document.getElementById('divPopUp');
 const divBlur = document.getElementById('divBlur');
+const musicToggle = document.getElementById('divMusic').childNodes[1];
+const imgs = document.getElementsByTagName('IMG');
+musicToggle.addEventListener('click', toggleMusic);
 document.getElementById('txtDropDown').addEventListener("click", toggleDropDown);
 window.addEventListener('resize', setPlaceHolderHeights);
-divPopUp.addEventListener('click', scrnUnblur);
+window.addEventListener('orientationchange', setPlaceHolderHeights);
+divBlur.addEventListener('click', scrnUnblur);
+for(i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', enlargeImg);
+}
 function toggleDropDown() {
-  if(mediaQuery.matches) {
-    let links = document.getElementById('divLinks').childNodes;
-    let state = window.getComputedStyle(links[1]).display;
-    if(state == 'none') {
-      state = 'block';
-    }else if(state == 'block') {
-      state = 'none';
-    }
-    for(i = 1; i < links.length; i += 2) {
-      links[i].style.display = state;
-    }
+  let links = document.getElementById('divLinks').childNodes;
+  let state = window.getComputedStyle(links[1]).display;
+  if(state == 'none') {
+    state = 'block';
+  }else if(state == 'block') {
+    state = 'none';
+  }
+  for(i = 1; i < links.length; i += 2) {
+    links[i].style.display = state;
   }
 }
 function setPlaceHolderHeights() {
@@ -27,13 +32,21 @@ function setPlaceHolderHeights() {
 function scrnUnblur() {
   divPopUp.style.display = 'none';
   divBlur.style.display = 'none';
-  audio.play();
+  imgEnlarged.src = '';
+  imgEnlarged.margin = '';
 }
-function audioCheck() {
-  if(audio.paused) {
-    divPopUp.style.display = 'block';
-    divBlur.style.display = 'block';
+function toggleMusic() {
+  if(musicToggle.checked) {
+    audio.play();
+  } else {
+    audio.pause();
   }
 }
+function enlargeImg() {
+  imgEnlarged.src = this.src;
+  divPopUp.style.display = 'block';  
+  divBlur.style.display = 'block';
+  imgEnlarged.style.margin = (((window.innerHeight - parseInt(window.getComputedStyle(imgEnlarged).height.split('p').shift()))/2)-1).toString() + 'px ' + ((((window.innerWidth - parseInt(window.getComputedStyle(imgEnlarged).width.split('p').shift()))/2)-1).toString() + 'px');
+}
 setPlaceHolderHeights();
-setTimeout(audioCheck, 1000);
+setTimeout(toggleMusic, 1000);
