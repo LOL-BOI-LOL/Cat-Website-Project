56d4668ec5496f4adb757c543f93cbe5a5bb6d37
Boi <51324966+LOL-BOI-LOL@users.noreply.github.com>
2021-11-17 02:39:29 +0000

    I changed how the spaces at the top and bottom work, which make it seem as if the title and sources are affecting the flow of the page. This system should be better and based around the actual height of the title and sources.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const mediaQuery = window.matchMedia('(max-width: 599px)');
+const divTopPH = document.getElementById('divTitlePlaceHolder');
+const divBottomPH = document.getElementById('divSourcePlaceHolder');
+const divTop = document.getElementById('divTop');
+const divBottom = document.getElementById('txtSource');
 document.getElementById('txtDropDown').addEventListener("click", toggleDropDown);
+window.addEventListener('resize', setPlaceHolderHeights);
 function toggleDropDown() {
   if(mediaQuery.matches) {
-    links = document.getElementById('divLinks').childNodes;
-    state = window.getComputedStyle(links[1]).display;
+    let links = document.getElementById('divLinks').childNodes;
+    let state = window.getComputedStyle(links[1]).display;
     if(state == 'none') {
       state = 'block';
     }else if(state == 'block') {
@@ -14,3 +19,8 @@ function toggleDropDown() {
     }
   }
 }
+function setPlaceHolderHeights() {
+  divTopPH.style.height = (100 * (parseInt(window.getComputedStyle(divTop).height.split('p').shift()) + 20) / window.innerWidth).toString() + 'vw';
+  divBottomPH.style.height = (100 * ((parseInt(window.getComputedStyle(divBottom).height.split('p').shift()) + 2 * parseInt(window.getComputedStyle(divBottom).paddingBottom.split('p').shift()))) / window.innerWidth).toString() + 'vw';
+}
+setPlaceHolderHeights();
