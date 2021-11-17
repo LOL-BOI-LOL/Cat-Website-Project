b304c7e1a6ccbc0934ce472f1280277cfa6cebdc
Boi <51324966+LOL-BOI-LOL@users.noreply.github.com>
2021-11-17 03:06:25 +0000

    Removed footer from pages and added a separate page for sources instead. This was to fix the problem of overcrowding the screens of devices with small heights.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const mediaQuery = window.matchMedia('(max-width: 599px)');
 const divTopPH = document.getElementById('divTitlePlaceHolder');
-const divBottomPH = document.getElementById('divSourcePlaceHolder');
 const divTop = document.getElementById('divTop');
-const divBottom = document.getElementById('txtSource');
 document.getElementById('txtDropDown').addEventListener("click", toggleDropDown);
 window.addEventListener('resize', setPlaceHolderHeights);
 function toggleDropDown() {
@@ -14,13 +12,12 @@ function toggleDropDown() {
     }else if(state == 'block') {
       state = 'none';
     }
-    for(i = 1; i < 6; i += 2) {
+    for(i = 1; i < links.length; i += 2) {
       links[i].style.display = state;
     }
   }
 }
 function setPlaceHolderHeights() {
   divTopPH.style.height = (100 * (parseInt(window.getComputedStyle(divTop).height.split('p').shift()) + 20) / window.innerWidth).toString() + 'vw';
-  divBottomPH.style.height = (100 * ((parseInt(window.getComputedStyle(divBottom).height.split('p').shift()) + 2 * parseInt(window.getComputedStyle(divBottom).paddingBottom.split('p').shift()))) / window.innerWidth).toString() + 'vw';
 }
 setPlaceHolderHeights();
